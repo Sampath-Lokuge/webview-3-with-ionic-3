43e867413384acfb77cdc53046f644df845c4e85
Sampath-Lokuge <sugeesampathlokuge@gmail.com>
2019-01-20 03:29:12 +0530

    init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private webview: WebView, private platform: Platform) {
 
+  }
+
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      const img = this.webview.convertFileSrc('file:///Users/dan/camera-image-12345.png');
+    });
   }
 
 }
